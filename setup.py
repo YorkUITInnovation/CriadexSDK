b7e530855fef797d69851f7343fcc0b5f5759db8
isaackogan <isaacikogan@gmail.com>
2024-07-18 01:03:43 -0400

    Bump Top K/N lims

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="CriadexSDK",
     packages=setuptools.find_packages(),
-    version="1.2.2",
+    version="1.2.5",
     description="Criadex SDK Client",
     author="Isaac Kogan",
     author_email="koganisa@yorku.ca",
